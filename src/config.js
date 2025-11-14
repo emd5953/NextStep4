96607348d61ca4f4b2bb59db20fe7f0afae1555e
EnrinJr <nrndbrma2@gmail.com>
2025-11-13 23:36:46 -0500

    bruh im tired

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 //export const API_SERVER = 'http://localhost:4000'; 
-export const API_SERVER = 'https://nextstep4-api.onrender.com'; 
+export const API_SERVER = 'https://nextstep4-api.onrender.com/api'; 
